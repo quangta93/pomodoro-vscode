e4e350cd6e628294e16b54b6f8e7408c1e407e50
Quang Tran <quangta93@gmail.com>
2023-11-19 18:41:53 -0500

    setup status bar items

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,69 @@
-import * as vscode from "vscode";
+import {
+  StatusBarAlignment,
+  commands,
+  window,
+  type ExtensionContext,
+} from "vscode";
 import { Pomodoro } from "./pomodoro";
 
-export function activate(context: vscode.ExtensionContext) {
-  const timer = new Pomodoro();
+enum TimerCommand {
+  start = "pomodoro-vscode.start",
+  stop = "pomodoro-vscode.stop",
+  skip = "pomodoro-vscode.skip",
+}
 
+const registerCommands = (
+  { subscriptions }: ExtensionContext,
+  timer: Pomodoro
+) => {
+  subscriptions.push(
+    commands.registerCommand(TimerCommand.start, () => {
+      timer.start();
+      window.showInformationMessage("Start");
+    })
+  );
+
+  subscriptions.push(
+    commands.registerCommand(TimerCommand.stop, () => {
+      timer.stop();
+      window.showInformationMessage("Stop");
+    })
+  );
+
+  subscriptions.push(
+    commands.registerCommand(TimerCommand.skip, () => {
+      timer.skip();
+      window.showInformationMessage("Skip");
+    })
+  );
+};
+
+const renderStatusBarItems = (context: ExtensionContext, timer: Pomodoro) => {
+  const skipBtn = window.createStatusBarItem(StatusBarAlignment.Right, 100);
+  skipBtn.command = TimerCommand.start;
+  skipBtn.text = "$(debug-step-over)";
+  skipBtn.tooltip = "Skip pomodoro period";
+  skipBtn.show();
+
+  const startBtn = window.createStatusBarItem(StatusBarAlignment.Right, 101);
+  startBtn.command = TimerCommand.skip;
+  startBtn.text = "$(play)";
+  startBtn.tooltip = "Start pomodoro timer";
+  startBtn.show();
+
+  const timerText = window.createStatusBarItem(StatusBarAlignment.Right, 102);
+  timerText.text = "00:00";
+  timerText.tooltip = "Pomodoro";
+  timerText.show();
+};
+
+export function activate(context: ExtensionContext) {
   console.log("pomodoro activated!");
 
-  const startDisposable = vscode.commands.registerCommand(
-    "pomodoro-vscode.start",
-    () => {
-      timer.start();
-      vscode.window.showInformationMessage("Start");
-    }
-  );
+  const timer = new Pomodoro();
 
-  const stopDisposable = vscode.commands.registerCommand(
-    "pomodoro-vscode.stop",
-    () => {
-      timer.stop();
-      vscode.window.showInformationMessage("Stop");
-    }
-  );
-
-  const skipDisposable = vscode.commands.registerCommand(
-    "pomodoro-vscode.skip",
-    () => {
-      timer.skip();
-      vscode.window.showInformationMessage("Skip");
-    }
-  );
-
-  const consoleDisposable = vscode.commands.registerCommand(
-    "pomodoro-vscode.console",
-    () => {
-      vscode.window.showInformationMessage(timer.toString());
-    }
-  );
-
-  context.subscriptions.push(startDisposable, stopDisposable, skipDisposable);
+  registerCommands(context, timer);
+  renderStatusBarItems(context, timer);
 }
 
 export function deactivate() {}
